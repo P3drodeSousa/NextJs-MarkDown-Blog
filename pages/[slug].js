65c2edae25f97fc29dbb4d75a6733ddd657c973c
Pedro De SOUSA <pedro.desousa@emploilausanne.ch>
2020-12-10 15:02:32 +0100

    Inital commit

---
+++ pages/[slug].js
@@ -0,0 +1,27 @@
+import { getAllPost, getPostBySlug } from "./api/posts";
+import Blog from "../components/Blog";
+
+export default function Post({ title, description, content }) {
+  return <Blog title={title} description={description} content={content} />;
+}
+
+export async function getStaticProps(context) {
+  return {
+    props: await getPostBySlug(context.params.slug)
+  };
+}
+
+export async function getStaticPaths() {
+  let paths = await getAllPost();
+
+  paths = paths.map(post => {
+    return {
+      params: { slug: post.slug }
+    };
+  });
+
+  return {
+    paths: paths,
+    fallback: false
+  };
+}
