3b4a4a562383207a341717b7f0915af04428d84f
Pedro De SOUSA <pedro.desousa@emploilausanne.ch>
2020-12-10 15:49:57 +0100

    Images generated

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -28,7 +28,7 @@ export async function getPostBySlug(slug) {
   const baseUrl =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://next-js-mark-down-blog.vercel.app/";
+      : "https://next-js-mark-down-blog.vercel.app";
 
   const meta = matter(fileContent.default);
   const content = marked(meta.content);
